fb83aa201f0709c5e7711b03a9217ae556aa0e13
Heitor A Neto <heitorh3@gmail.com>
2018-11-27 11:09:55 -0200

    Autenticação JWT

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const UserController = require('./app/controllers/UserController')
+const SessionCOntroller = require('./app/controllers/SessionController')
 
 const routes = express.Router()
 
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionCOntroller.store)
 
 module.exports = routes
