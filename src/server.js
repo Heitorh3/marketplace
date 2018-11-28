9eedab28fb24f03aec41d0b3a40d289053a89a53
Heitor A Neto <heitorh3@gmail.com>
2018-11-28 11:36:17 -0200

    Exception Handling

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const youch = require('youch')
+const validate = require('express-validation')
+
 const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
-    // this.isDev = process.env.NODE_ENV != 'production'
+    this.isDev = process.env.NODE_ENV != 'production'
 
     this.database()
     this.middlewares()
     this.routes()
+    this.exception()
   }
 
   database () {
@@ -28,6 +32,23 @@ class App {
 
   routes () {
     this.express.use(require('./routes'))
+  }
+
+  exception () {
+    this.express.use(async (err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.status).json(err)
+      }
+
+      if (this.isDev) {
+        const youch = new Youch(err, req)
+        return res.json(await youch.toJSON())
+      }
+
+      return res
+        .status(err.status || 500)
+        .json({ error: 'Internal Server Error' })
+    })
   }
 }
 
